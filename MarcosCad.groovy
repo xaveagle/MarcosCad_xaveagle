dbdcc5d46a5f3c8c84dc49d096278eeb5ac28b5c
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-12 10:32:53 -0400

    updated cad generator

--- MarcosCad.groovy
+++ MarcosCad.groovy
@@ -1,38 +1,66 @@
 import com.neuronrobotics.bowlerstudio.creature.ICadGenerator
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
-
-// code here
+File parametricsCSV = ScriptingEngine.fileFromGit("https://github.com/OperationSmallKat/Marcos.git", "parametrics.csv")
+HashMap<String,String> measurments=new HashMap<>()
+HashMap<String,Double> numbers;
+BufferedReader reader;
+String code="HashMap<String,Double> numbers = new HashMap<>()\n"
+try {
+	reader = new BufferedReader(new FileReader(parametricsCSV.getAbsolutePath()));
+	String line = reader.readLine();
+	while ((line = reader.readLine() )!= null) {
+		if(line.length()>3) {
+			//System.out.println(line);
+			String[] parts = line.split(",");
+			String reconstructed =parts[0]+"="+parts[2].replaceAll("mm", "");
+			measurments.put(parts[0], reconstructed)
+			code+= reconstructed+"\n"
+			code+="numbers.put(\""+parts[0]+"\","+parts[0]+");\n"
+		}
+	}
+	reader.close();
+} catch (IOException e) {
+	e.printStackTrace();
+}
+code+="return numbers"
+println code
+numbers=(HashMap<String,Double>) ScriptingEngine.inlineScriptStringRun(code, null, "Groovy");
+for(String key :numbers.keySet()) {
+	println key+" : "+numbers.get(key)
+}
 
 return new ICadGenerator(){
 
-	@Override
-	public ArrayList<CSG> generateCad(DHParameterKinematics arg0, int arg1) {
-		// TODO Auto-generated method stub
-		ArrayList<CSG> back =[]
-		back.add(new Cube(1).toCSG())
-		for(CSG c:back)
-			c.setManipulator(arg0.getLinkObjectManipulator(arg1))
-		return back;
-	}
+			@Override
+			public ArrayList<CSG> generateCad(DHParameterKinematics arg0, int arg1) {
 
-	@Override
-	public ArrayList<CSG> generateBody(MobileBase arg0) {
-		// TODO Auto-generated method stub
-		ArrayList<CSG> back =[]
-		back.add(new Cube(1).toCSG())
-		for(CSG c:back)
-			c.setManipulator(arg0.getRootListener())
-		for(DHParameterKinematics kin:arg0.getAllDHChains()) {
-			CSG limbRoot =new Cube(1).toCSG()
-			limbRoot.setManipulator(kin.getRootListener())
-			back.add(limbRoot)
+
+				ArrayList<CSG> back =[]
+				back.add(new Cube(1).toCSG())
+				for(CSG c:back)
+					c.setManipulator(arg0.getLinkObjectManipulator(arg1))
+				return back;
+			}
+
+			@Override
+			public ArrayList<CSG> generateBody(MobileBase arg0) {
+				// TODO Auto-generated method stub
+				ArrayList<CSG> back =[]
+				back.add(new Cube(1).toCSG())
+				for(CSG c:back)
+					c.setManipulator(arg0.getRootListener())
+				for(DHParameterKinematics kin:arg0.getAllDHChains()) {
+					CSG limbRoot =new Cube(1).toCSG()
+					limbRoot.setManipulator(kin.getRootListener())
+					back.add(limbRoot)
+				}
+				return back;
+			}
+
+
 		}
-		return back;
-	}
-	
-	
-}
